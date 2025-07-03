d4f844836b43556db5f2bc125f100c5bc71157ca
orderlymirror <141560010+orderlymirror@users.noreply.github.com>
2025-07-03 15:49:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ Your Flask App is LIVE on Azure!"
+    return "app is now live on azure"
 
 if __name__ == '__main__':
     app.run()
